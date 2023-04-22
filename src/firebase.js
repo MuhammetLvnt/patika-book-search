a56304a8cd78ded53e11630748831593feb57000
MuhammetLvnt <79282877+MuhammetLvnt@users.noreply.github.com>
2023-04-22 19:27:07 +0300

    env added

--- src/firebase.js
+++ src/firebase.js
@@ -12,12 +12,12 @@ import { userHandle } from "./utils";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD4VgPjO66QVSWDIrIpUzcsgUp-ODlroGw",
-  authDomain: "ml-bookapp-81723.firebaseapp.com",
-  projectId: "ml-bookapp-81723",
-  storageBucket: "ml-bookapp-81723.appspot.com",
-  messagingSenderId: "375179355897",
-  appId: "1:375179355897:web:9d451100e856ea9fb76428",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 const app = initializeApp(firebaseConfig);
 // auth oluşturuyoruz
